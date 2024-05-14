8702be01709e81988a3fa8c076d26c0a5bead9fb
Alysson-b <161394750+Alysson-b@users.noreply.github.com>
2024-05-13 21:35:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let name = (prompt("preencha a tabela"))
+var table = document.getElementById('linha1').innerText = name;
+
+let adicionar = prompt("adicionar nome");
+ var table = document.getElementById('linha2').innerText = adicionar;
+
+ let tabela = prompt("adicionar nome");
+ var table = document.getElementById('linha3').innerText = tabela;
+
+ let coll1 = prompt("adicionar nome");
+ var table = document.getElementById('coll1').innerText = coll1;
+
+ let coll2 = prompt("adicionar nome");
+ var table = document.getElementById('coll2').innerText = coll2;
+
+ let coll3 = prompt("adicionar nome");
+ var table = document.getElementById('coll3').innerText = coll3;
+
+
